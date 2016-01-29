1eff85ccb27eb8f2353cee5c3bdf624a8af2c31c
Avneet Kour <avneet.aryans@gmail.com>
2016-01-29 12:05:59 +0530

    Methods practice

--- index.php
+++ index.php
@@ -2,8 +2,43 @@
 	<head>
 		<title> j query </title>
 		<script src= "jquery-2.2.0.min.js"></script>
+		<script>
+			window.onload = function() {
+			  console.log('avneet');
+			}
+		</script>
+		<script>
+			$(document).ready( 
+				function() {
+					$('#btn').click(
+						function() {
+							$(this).fadeOut().fadeIn().css('color','orange');
+						}
+					);
+					
+					
+					$('#btn').click(
+						function() {
+							console.log( $('#btn2').val('Dont press me I will pop') );
+						}
+					);
+					
+					$('#btn2').click(
+						function() {
+							$(this).fadeOut();
+							$(this).fadeIn();
+							$(this).css('color','orange');
+						}
+					);
+				}
+			
+			);
+		</script>
 	</head>
 	<body>
-	
+		<button id= "btn">
+		  Press Me
+		</button>
+		<input type="button" id="btn2" value="Press Me"/>
 	</body>
 </html>
---
+++ methods.php
@@ -0,0 +1,35 @@
+<html>
+	<head>
+		<title>
+		</title>
+	</head>
+	
+	<body>
+		<div class="red">
+			<ul class="small">
+				<li>abcd</li>
+				<li>efgh
+					<ul>
+						<li>e</li>
+						<li>f</li>
+						<li>g</li>
+						<li>h</li>
+					</ul>
+				</li>
+			</ul>
+			<ul class="big">
+				<li>ABCD</li>
+				<li>EFGH</li>
+			</ul>
+		</div>
+		
+		<div class="blue">
+			<form>
+				<input type='button' class="btn" id="btn1" value="Click Here" checked><br />
+				<input type='button' class="btn" id="btn2" value="Click Here"><br />
+				<input type='text' class="text" id="text1" value="Hello"><br />
+				<input type='button' class="text" id="text2" value="How are you?"><br />
+			</form>
+		</div>
+	</body>
+</html>
