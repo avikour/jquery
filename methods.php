c8a070088e287afe74dbb01b86a92ac61676c4a6
Avneet Kour <avneet.aryans@gmail.com>
2016-02-02 14:26:37 +0530

    Task 4

--- methods.php
+++ methods.php
@@ -10,6 +10,10 @@
 	=============================  Task 3  =============================
       used .length and if
 	  add 'li' atmost twice to the selected ul
+
+	=============================  Task 4   =============================
+      used function(event)  .preventDefault .parent .attr
+	  add  dynamic id to the new li, li should have an anchor link to google.com but shoudn't redirect and display 		  its parent's id in console.log when clicked on
 -->
 
 <html>
@@ -28,15 +32,22 @@
 					$('#btn2').on('click', 
 					  	function() { 
 							var x = $( ".big li" ).length;
+					
 							if (x < 4) {
-								$( ".big" ).append('<li>IJKL</li>');
+								$( ".big" ).append('<li id="li_'+ (x+1) +'"><a class="my_link" href="https://google.com">I\'m new li</a></li>');
 							}
-						    $("ul.big li").eq( 1 ).css( "background-color", "yellow" );
+						    $("ul.big li").eq( 1 ).css( "background-color", "yellow" );	
 					}
 					 );
+					
+					$('.big').on('click', '.my_link',
+					  	function(e){
+							e.preventDefault();
+							console.log($(this).parent().attr('id'));
+						}
+					);
 				}
 			);
-			
 		</script>
 		
 	</head>
@@ -65,7 +76,7 @@
 				<input type='button' class="btn" id="btn1" value="For Task 1 Click Here" checked><br />
 				<input type='button' class="btn" id="btn2" value="For Task 2 Click Here"><br />
 				<input type='text' class="text" id="text1" value="Hello"><br />
-				<input type='button' class="text" id="text2" value="How are you?"><br />
+				<input type='text' class="text" id="text2" value="How are you?"><br />
 			</form>
 		</div>
 	</body>
